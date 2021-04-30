2fe5787e971e2b56e63255e2f3b76b77ed820c5f
Alessandro <cikkeale@gmail.com>
2021-04-30 19:25:51 +0200

    General commit

--- docs/NoRepetition.java
+++ docs/NoRepetition.java
@@ -8,7 +8,7 @@ class NoRepetition {
         int check = 0;
         for (int i = 0; i < a.length; i++) {
             boolean z = true;
-            while (z == true) {
+            while (z) {
                 check = Math.abs(rnd.nextInt() % 10);
                 for (int i2 = 0; i2 < 5; i2++) {
                     if (check == a[i2]) {
