e3940d37d456c425111361a2d3c5b4eaf19fbe8a
Alessandro <cikkeale@gmail.com>
2021-05-01 18:50:42 +0200

    General commit

---
+++ docs/TrovaValoreUguale.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//Scrivere un programma che verifica se esistono due elementi uguali nell'array.
+public class TrovaValoreUguale {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        boolean end = true;
+        System.out.println("Inserisci 10 numeri interi:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int i2 = 0; i2 < array.length; i2++) {
+                if(i==i2){
+                }else if(array[i] == array[i2]){
+                    end = false;
+                    break;
+                }
+            }
+            if(!end){
+                break;
+            }
+        }
+        if(!end){
+            System.out.println("Ci sono 2 numeri uguali.");
+        }else{
+            System.out.println("Non ci sono numeri uguali.");
+        }
+    }
+}
